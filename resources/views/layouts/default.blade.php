5e7c9ba6ab27739736566f8d29b41738b1c95888
man <3398284534@qq.com>
2018-06-21 02:26:19 +0000

    code format

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','blogByMan') - blogv1</title>
+    <title>@yield('title', 'Blog 2018') - Blog 2018</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
- @yield('content')
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="/" id="logo">Man@2018</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/help">帮助</a></li>
+                    <li><a href="#">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+    @yield('content')
+</div>
 </body>
 </html>
