906d976ceb288d79a00707850696bf0221599824
man <3398284534@qq.com>
2018-06-22 01:43:43 +0000

    add user info show for signal user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller {
     /**
@@ -11,5 +12,14 @@ class UsersController extends Controller {
      */
     public function create() {
         return view('users.create');
+    }
+    
+    /**
+     * 用户个人信息展示页
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user) {
+        return view('users.show', compact('user'));
     }
 }
