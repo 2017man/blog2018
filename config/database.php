914d102e2039d4fe19cd487b33e1b8acc0e6077a
man <3398284534@qq.com>
2018-06-22 03:51:36 +0000

    updateted database configuration when server in heruku and localhost

--- config/database.php
+++ config/database.php
@@ -1,7 +1,8 @@
 <?php
 
+$db_config = get_db_config();
 return [
-
+    
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -12,9 +13,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
-
+    
+    'default' => $db_config['connection'],
+    
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -30,15 +31,15 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-
+    
     'connections' => [
-
+        
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-
+        
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -53,20 +54,21 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
+        
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
-
+        
+        
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
@@ -77,9 +79,9 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
-
+    
     ],
-
+    
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
@@ -90,9 +92,9 @@ return [
     | the migrations on disk haven't actually been run in the database.
     |
     */
-
+    
     'migrations' => 'migrations',
-
+    
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -103,18 +105,18 @@ return [
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
-
+    
     'redis' => [
-
+        
         'client' => 'predis',
-
+        
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
-
+    
     ],
 
 ];
