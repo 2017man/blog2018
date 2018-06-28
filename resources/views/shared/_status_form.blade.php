41ec354e1860e27e0d7ad062ba63aaddb26751da
man <3398284534@qq.com>
2018-06-28 01:41:59 +0000

    update ueeditor

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -1,14 +1,14 @@
 <form action="{{ route('statuses.store') }}" method="POST">
-
+    @include('vendor.ueditor.assets')
     @include('shared._errors')
     {{ csrf_field() }}
 
     {{--<textarea id="container" class="form-control" rows="3" placeholder="聊聊新鲜事儿..." name="content">{{ old('content') }}</textarea>--}}
     <div id="ueditor" class="col-lg-12">
-        @include('UEditor::head')
-        <script id="container" name="content" type="text/plain">
-    这里写你的博客内容...(功能还在完善中...)
-        </script>
+
+    <!-- 编辑器容器 -->
+        <script id="container" name="content" type="text/plain">{{ old('content') }}</script>
+
 
     </div>
     <button type="submit" class="btn btn-primary pull-right">发布</button>
@@ -19,5 +19,10 @@
 <!-- 实例化编辑器 -->
 <script type="text/javascript">
     var ue = UE.getEditor('container');
+    ue.ready(function () {
+        ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
+    });
 </script>
+
+
 
