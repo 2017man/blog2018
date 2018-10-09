f1dc0621511f0a063b9c49400865755c0791f361
man <3398284534@qq.com>
2018-10-09 15:16:55 +0000

    fix bug

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -4,25 +4,12 @@
     {{ csrf_field() }}
 
     <textarea id="container" class="form-control" rows="3" placeholder="聊聊新鲜事儿..." name="content">{{ old('content') }}</textarea>
-    {{--<div id="ueditor" class="col-lg-12">--}}
 
-    <!-- 编辑器容器 -->
-        {{--<script id="container" name="content" type="text/plain">{{ old('content') }}</script>--}}
-
-
-    </div>
     <button type="submit" class="btn btn-primary pull-right">发布</button>
 
 </form>
 
 
-<!-- 实例化编辑器 -->
-{{--<script type="text/javascript">--}}
-    {{--var ue = UE.getEditor('container');--}}
-    {{--ue.ready(function () {--}}
-        {{--ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.--}}
-    {{--});--}}
-{{--</script>--}}
 
 
 
