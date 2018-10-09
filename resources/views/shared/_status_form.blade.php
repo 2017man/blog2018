9c99fd4bce75a718a981b9e434adbf87add233d8
man <3398284534@qq.com>
2018-10-09 15:13:30 +0000

    test

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -3,11 +3,11 @@
     @include('shared._errors')
     {{ csrf_field() }}
 
-    {{--<textarea id="container" class="form-control" rows="3" placeholder="聊聊新鲜事儿..." name="content">{{ old('content') }}</textarea>--}}
-    <div id="ueditor" class="col-lg-12">
+    <textarea id="container" class="form-control" rows="3" placeholder="聊聊新鲜事儿..." name="content">{{ old('content') }}</textarea>
+    {{--<div id="ueditor" class="col-lg-12">--}}
 
     <!-- 编辑器容器 -->
-        <script id="container" name="content" type="text/plain">{{ old('content') }}</script>
+        {{--<script id="container" name="content" type="text/plain">{{ old('content') }}</script>--}}
 
 
     </div>
@@ -17,12 +17,12 @@
 
 
 <!-- 实例化编辑器 -->
-<script type="text/javascript">
-    var ue = UE.getEditor('container');
-    ue.ready(function () {
-        ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
-    });
-</script>
+{{--<script type="text/javascript">--}}
+    {{--var ue = UE.getEditor('container');--}}
+    {{--ue.ready(function () {--}}
+        {{--ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.--}}
+    {{--});--}}
+{{--</script>--}}
 
 
 
--- resources/views/statuses/create.blade.php
+++ resources/views/statuses/create.blade.php
@@ -3,7 +3,11 @@
 @section('title','发布博客')
 @section('content')
     <div class="row">
-        
+        <div class="col-lg-12">
+            <section class="status_form">
+                @include('shared._status_form')
+            </section>
+        </div>
     </div>
 
 @stop
