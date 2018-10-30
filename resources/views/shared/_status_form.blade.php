50d5f737f22bad07ad23dec42851cadffd7b41e7
man <3398284534@qq.com>
2018-10-30 10:07:27 +0000

    fix blog publish page style

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -2,7 +2,7 @@
     @include('shared._errors')
     {{ csrf_field() }}
     <div id="mdeditor">
-      <textarea id="container" class="form-control" name="content" placeholder="聊聊新鲜事儿..." style="width:100%;display:none;">{{ old('content') }}</textarea>
+      <textarea id="container" class="form-control" name="content" placeholder="聊聊新鲜事儿..." style="display:none;">{{ old('content') }}</textarea>
     </div>
 
     <button type="submit" class="btn btn-primary pull-right">发布</button>
